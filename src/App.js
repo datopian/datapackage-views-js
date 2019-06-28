4625d4f3542c9522ea38f01c282795d2f913e53a
anuveyatsu <anuar.ustayev@gmail.com>
2019-06-28 18:23:08 +0600

    [data load/view compile][m]: implemented loading Dataset and files + compiling views - refs #2 and #3
    TODO: remove hard-coded test 'DP_ID' var

--- src/App.js
+++ src/App.js
@@ -2,41 +2,21 @@ import React from "react"
 import "./App.css"
 import Table from "./Table.js"
 
-function App(props) {
-  let data
-  let views = [{}] // default to single table view
+async function App(props) {
+  let data = props.view ? props.view.resources[0].data : []
+  let view = props.view || {} // default to single table view
 
-  // @@TODO encapsulate / clean up datapackage validation
-  try {
-    data = props.datapackage._descriptor.data
-  } catch (e) {
-    data = {}
-    console.warn("datapackage.json does not include data or is not valid", e)
+  let renderedView = <p>Data view unavailable</p>
+  if (view.specType === "table" || !view.specType) {
+    renderedView = <Table data={data} />
   }
-
-  try {
-    const confViews = props.datapackage._descriptor.views
-    views = confViews ? confViews : [{}]
-  } catch (e) {
-    console.log("No views found in datapackage descriptor")
-  }
-
-  console.log(views)
-
-  const renderedViews = views.map((view, i) => {
-    console.log("VUEW", view)
-    if (view.type === "table" || !view.type) {
-      return <Table data={data} key={i} />
-    }
-    return <p>Data view unavailable</p>
-  })
 
   return (
     <div className="App">
       <header className="h-6 bg-gray-300">
         <h1>Datapackage Views</h1>
       </header>
-      <div className="container m-24">{renderedViews}</div>
+      <div className="container m-24">{renderedView}</div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as dpRender from 'datapackage-render'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const { Dataset } = require("data.js")
+const toArray = require("stream-to-array")
+
+// Read DP_ID from global scope
+const DP_ID = {
+  "description": "Country, regional and world GDP.",
+  "name": "gdp",
+  "title": "Country, Regional and World GDP (Gross Domestic Product)",
+  "resources": [
+    {
+      "name": "gdp",
+      "path": "https://pkgstore.datahub.io/core/gdp/gdp_csv/data/0048bc8f6228d0393d41cac4b663b90f/gdp_csv.csv",
+      "schema": {
+        "fields": [
+          {
+            "name": "Country Name",
+            "type": "string"
+          },
+          {
+            "name": "Country Code",
+            "type": "string"
+          },
+          {
+            "name": "Year",
+            "type": "year"
+          },
+          {
+            "description": "GDP in current USD",
+            "name": "Value",
+            "type": "number"
+          }
+        ]
+      }
+    }
+  ],
+  "views": [
+    {
+      "elementId": "root",
+      "title": "GDP",
+      "resources": ["gdp"],
+      "specType": "table"
+    }
+  ]
+}
+// Load Dataset object
+Dataset.load(DP_ID).then(async (dataset) => {
+  // TODO: support local files
+  // Convert remote file into inline file
+  await Promise.all(dataset.resources.map(async (file) => {
+    if (file.displayName === "FileRemote") {
+      const rowStream = await file.rows({size: 20})
+      const data = await toArray(rowStream)
+      file.descriptor.data = data // This makes it FileInline
+    }
+  }))
+
+  // Compile views and render App
+  dataset.descriptor.views.forEach(view => {
+    const compiledView = dpRender.compileView(view, dataset.descriptor)
+    ReactDOM.render(
+      <App view={compiledView} />,
+      document.getElementById(compiledView.elementId)
+    );
+  })
+
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
