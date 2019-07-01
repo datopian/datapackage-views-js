5d6aca0a97a7537b5569457d34cecb71e66ecb18
anuveyatsu <anuar.ustayev@gmail.com>
2019-07-01 13:25:42 +0600

    [tests][l]: setup essential tests with 3 cases: data loading, failed to load and loaded - refs #1 and #2

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
+
+const view = {
+  'id': 1,
+  'elementId': 'root',
+  'title': 'GDP',
+  'resources': [
+    'gdp'
+  ],
+  'specType': 'table'
+}
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const div = document.createElement('div')
+  ReactDOM.render(<App view={view} />, div)
+  ReactDOM.unmountComponentAtNode(div)
 });
+
+it('renders loading message', () => {
+  const { getByText } = render(<App view={view} />)
+  expect(getByText('Data view is loading')).toBeInTheDocument()
+});
+
+it('renders error message when data is unavailable', () => {
+  const errorView = JSON.parse(JSON.stringify(view))
+  errorView.resources[0] = {
+    name: 'gdp',
+    unavailable: true
+  }
+  const { getByText } = render(<App view={errorView} />)
+  expect(getByText('Data view unavailable.')).toBeInTheDocument()
+})
+
+it('renders a preview table when data is loaded', () => {
+  const compiledView = JSON.parse(JSON.stringify(view))
+  compiledView.resources[0] = {
+    name: 'gdp',
+    data: [
+      {a:1, b:2},
+      {a:3, b:4}
+    ]
+  }
+  const { container } = render(<App view={compiledView} />)
+  expect(container.firstChild).toMatchSnapshot()
+})
