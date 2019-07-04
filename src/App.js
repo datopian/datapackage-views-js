1b8ab0f96f038401cf59d1cd6213c4df86ba5244
anuveyatsu <anuar.ustayev@gmail.com>
2019-07-04 18:31:53 +0600

    [tidy up][xs]: improve logic and remove redundunt code + default zooming for the maps is 10

--- src/App.js
+++ src/App.js
@@ -6,23 +6,20 @@ import PdfViewer from './Document.js'
 import {handsOnTableToHandsOnTable} from 'datapackage-render'
 
 function App(props) {
-  let _data = props.view ? props.view.resources[0].data : []
-  if (_data && _data.length > 0) {
+  let _data = props.view ? props.view.resources[0].data : null
+  
+  if (props.view.specType === 'table' && _data) {
     let view = props.view || {} // default to single table view
     if (view.resources) view.resources[0]._values =  _data
     let {data, ...options} = handsOnTableToHandsOnTable(view)
-    let renderedView = <p>Data view unavailable</p>
-
-    if (view.specType === "table") {
-      renderedView = <Table data={data} options={options} />
-    }
-
     return (
       <div className="App">
-        <div className="container m-24">{renderedView}</div>
+        <div className="container m-24">
+          <Table data={data} options={options} />
+        </div>
       </div>
     )
-  } else if (_data && _data.type && props.view.specType === 'map') {
+  } else if (props.view.specType === 'map' && _data && _data.type) {
     return (
       <div className="App">
         <div className="container m-24">
