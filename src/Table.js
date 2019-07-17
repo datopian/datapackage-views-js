c08c4a1498235e4961abd00bab25dfdeccab22f1
anuveyatsu <anuar.ustayev@gmail.com>
2019-07-17 13:21:09 +0600

    [downloadables][m]: Table component allows users to download displayed data in CSV and JSON formats

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { HotTable } from "@handsontable/react"
+import { CSVLink } from "react-csv"
 import "./Table.css"
 
 
@@ -26,7 +27,47 @@ export default class Table extends React.Component {
     })
   }
 
+  downloadJson = () => {
+    let data = this.state.data
+    // If rows are arrays, we want to convert to objects:
+    if (data && Array.isArray(data[0])) {
+      const headers = this.state.settings.colHeaders
+      data = data.map(function(x) {
+        const row = {}
+        headers.forEach((header, i) => {
+          row[header] = x[i]
+        })
+        return row
+      })
+    }
+    data = JSON.stringify(data, null, 2)
+    const blob = new Blob([data], {type:'application/json'})
+    const link = document.createElement('a')
+    link.href = URL.createObjectURL(blob)
+    link.download = `${this.state.settings.viewTitle}.json`
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+
   render() {
-    return <HotTable data={this.state.data} width="100%" height="300" settings={this.state.settings} />
+    return (
+      <div>
+        <div className="downloadables">
+          <CSVLink
+            data={this.state.data}
+            headers={this.state.settings.colHeaders}
+            filename={`${this.state.settings.viewTitle}.csv`}
+            className="btn btn-primary"
+            target="_blank"
+          >
+            CSV (Excel)
+          </CSVLink>
+          <button onClick={this.downloadJson} className="btn btn-primary">JSON</button>
+        </div>
+
+        <HotTable data={this.state.data} width="100%" height="300" settings={this.state.settings} />
+      </div>
+    )
   }
 }
