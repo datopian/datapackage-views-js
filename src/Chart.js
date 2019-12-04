7f0cc738723a9aef4f2be06a538827d40b64c327
EvgeniiaVak <27793901+EvgeniiaVak@users.noreply.github.com>
2019-12-04 15:18:59 +0400

    [plotly][s] adds plotly specFeature/plotly spec - closes #33 (#34)
    * [npm][m] adds package-lock.json
    
    * [npm][s] executes npm update
    
    * [gitignore][xs] adds costmos-export to .gitignore
    
    * [plotly][s] adds plotly spec
    
    * [plotly][s] adds plotly fixture
    
    * [yarn][s] executes yarn upgrade datapackage-render
    
    * [plotly][s] adds plotly spec snapshot test

--- src/Chart.js
+++ src/Chart.js
@@ -6,6 +6,6 @@ import createPlotlyComponent from "react-plotly.js/factory";
 export default function (props) {
   const Plot = createPlotlyComponent(Plotly)
   return (
-    <Plot data={props.spec.data} layout={props.spec.layout} />
+    <Plot data={props.spec.data} layout={props.spec.layout} config={props.spec.config}/>
   )
 }
