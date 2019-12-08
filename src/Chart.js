93703f0f940d4688c3d46fbab5e0f3114471fdd6
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2019-12-08 18:17:12 +0400

    [plotly][s] removes produced with plotly icon

--- src/Chart.js
+++ src/Chart.js
@@ -4,7 +4,13 @@ import createPlotlyComponent from "react-plotly.js/factory";
 
 
 export default function (props) {
-  const Plot = createPlotlyComponent(Plotly)
+  const Plot = createPlotlyComponent(Plotly);
+
+  // removes produced with plotly logo by default
+  if (!props.spec.config || !props.spec.config.displaylogo) {
+    props.spec.config = Object.assign(props.spec.config || {}, {displaylogo: false});
+  }
+
   return (
     <Plot data={props.spec.data} layout={props.spec.layout} config={props.spec.config}/>
   )
