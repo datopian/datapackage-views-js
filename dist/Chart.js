e71a3e40290b443525c221a16c22264fd2d0d559
anuveyatsu <anuar.ustayev@gmail.com>
2020-03-19 13:10:27 +0600

    v1.3.15

--- dist/Chart.js
+++ dist/Chart.js
@@ -13,6 +13,8 @@ var _factory = _interopRequireDefault(require("react-plotly.js/factory"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
 function _default(props) {
   var Plot = (0, _factory.default)(_plotly.default); // removes produced with plotly logo by default
 
@@ -22,5 +24,14 @@ function _default(props) {
     });
   }
 
-  return _react.default.createElement(Plot, props.spec);
+  return _react.default.createElement(Plot, _extends({}, props.spec, {
+    layout: {
+      autosize: true
+    },
+    style: {
+      width: "100%",
+      height: "100%"
+    },
+    useResizeHandler: "true"
+  }));
 }
